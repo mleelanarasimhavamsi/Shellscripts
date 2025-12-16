ca462a083fb4892818a8720635263577c67239cb
vamsi <mleelanarasimhavamsi@gmail.com>
2025-12-16 06:07:26 +0530

    conversation msg

---
+++ conversation.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+
+
+
+echo "Farook":"Hi Dude how are you"
+echo "Friend":"Fine dude how about you"
+echo "Farook":"Ya good"
---
+++ variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash#!/bin/bash
+
+person1=farook
+person2=john
+
+
+echo "$person1:: Hi $person2 how are you"
+echo "$person2:: Fine $person1 how about you"
+echo "$person1:: Ya good"
