3d283907f45cdda2e66fbb3c741b0961fa4f5249
vamsi <mleelanarasimhavamsi@gmail.com>
2025-12-17 06:09:38 +0530

    dynamic values

--- specialcharecter.sh
+++ specialcharecter.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-echo "All variables passed to script:$@"
-echo "All variables passed to script:$*"
-echo "Script name:$0"
-echo "current directory:$pwd"
-echo "who is running this:$user"
+echo "All variables passed to script: $@"
+echo "All variables passed to script: $*"
+echo "Script name: $0"
+echo "current directory: $pwd"
+echo "who is running this: $user"
 echo "home directory:$home"
-echo "PID:$$"
+echo "PID: $$"
