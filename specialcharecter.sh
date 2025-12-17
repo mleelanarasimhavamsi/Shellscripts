080ea5496b25ff6fb91e8d60824aac0ae2f8834f
vamsi <mleelanarasimhavamsi@gmail.com>
2025-12-17 06:04:57 +0530

    dynamic values

--- specialcharecter.sh
+++ specialcharecter.sh
@@ -3,3 +3,7 @@
 echo "All variables passed to script:$@"
 echo "All variables passed to script:$*"
 echo "Script name:$0"
+echo "All variables passed to script:$pwd"
+echo "All variables passed to script:$user"
+echo "All variables passed to script:$home"
+echo "All variables passed to script:$$"
