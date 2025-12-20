539d9990201aad2c04246e7ee7e81306f2a075bb
vamsi <mleelanarasimhavamsi@gmail.com>
2025-12-20 21:21:08 +0530

    condition

--- condition.sh
+++ condition.sh
@@ -15,7 +15,7 @@
 echo "Please enter the number"
 read number
 
-if[ $(($number%2)) -eq 0]; then
+if[ $(($number % 2)) -eq 0 ]; then
     echo "Given $number is even"
 else
     echo "Given $number is odd"
