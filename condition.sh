6c23ddbecb154d3651b977cd455d967f06aa6522
vamsi <mleelanarasimhavamsi@gmail.com>
2025-12-20 21:19:45 +0530

    condition

--- condition.sh
+++ condition.sh
@@ -1,12 +1,23 @@
 #!/bin/bash
 
 
-number=$1
+# number=$1
 
-if [ $number -lt 10 ]; then
+# if [ $number -lt 10 ]; then
 
 
-    echo "Given number $number is lessthan 10"
+#     echo "Given number $number is lessthan 10"
+# else
+#     echo "Given number $number is greterthan 10"
+# fi
+
+
+echo "Please enter the number"
+read number
+
+if[ $(($number%2)) -eq 0]; then
+    echo "Given $number is even"
 else
-    echo "Given number $number is greterthan 10"
+    echo "Given $number is odd"
+
 fi
