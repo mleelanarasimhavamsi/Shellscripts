a0abd81376c63afdfef0f432cf9c389bbc84a912
vamsi <mleelanarasimhavamsi@gmail.com>
2025-12-20 21:12:52 +0530

    condition

--- condition.sh
+++ condition.sh
@@ -2,8 +2,11 @@
 
 
 number=$1
-if [$number -lt 10]; then
+
+if [ $number -lt 10 ]; then
+
+
     echo "Given number $number is lessthan 10"
 else
-    echo "Given number $number is greterhan 10"
+    echo "Given number $number is greterthan 10"
 fi
